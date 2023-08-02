552fe41b400f6b9401147f53e5c79520e7f0bb76
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-08-02 17:34:43 +0200

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -3,3 +3,13 @@ AddEventHandler('esx:playerLoaded', function(xPlayer)
   ESX.PlayerLoaded = true
   SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
 end)
+
+Citizen.CreateThread(function()
+	while true do
+		SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+		Citizen.Wait(100000)
+	end
+end)
+
+
+	 
